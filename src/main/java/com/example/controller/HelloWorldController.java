3b93d543663ebc22538aa5e2cf625b1ee57d9c3c
SamratChatterjee <33487722+SamratChatterjee@users.noreply.github.com>
2020-08-29 23:33:08 +0530

    Modifying the Controller

--- src/main/java/com/example/controller/HelloWorldController.java
+++ src/main/java/com/example/controller/HelloWorldController.java
@@ -1,13 +1,20 @@
 package com.example.controller;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
     @RequestMapping("/")
-    public String index() {
+    public String index(@RequestParam("id") String id) {
+        return "Greetings from Spring Boot!";
+    }
+
+    @RequestMapping("/books/{bookId}")
+    public String index1(@PathVariable("bookId") int bookId) {
         return "Greetings from Spring Boot!";
     }
 
